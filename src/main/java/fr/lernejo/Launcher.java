2f383b92d1b1eb35edb998f0d8a767748bf9550f
LE huu phuc <huu-phuc.le@epita.fr>
2022-03-15 16:09:49 +0100

    Partie 2 : Ping Hander

--- src/main/java/fr/lernejo/Launcher.java
+++ src/main/java/fr/lernejo/Launcher.java
@@ -1,7 +1,46 @@
 package fr.lernejo;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class Launcher {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+        final Map<String, String> gameContext = new HashMap<String, String>();
+        if (args.length < 1)
+            return;
+
+        int port = get_port(args);
+        UUID id = UUID.randomUUID();
+        gameContext.put("my_id", id.toString());
+        gameContext.put("my_port", String.valueOf(port));
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 1);
+        ExecutorService executorService =
+            new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+
+        server.createContext("/ping", new PingHandler());
+//        server.createContext("/api/game/start", new StartHandler(gameContext));
+//        server.createContext("/api/game/fire", new FireHandler());
+        server.setExecutor(executorService);
+        server.start();
+
+    }
+
+    private static int get_port(String[] args) {
+        try {
+            return Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 }

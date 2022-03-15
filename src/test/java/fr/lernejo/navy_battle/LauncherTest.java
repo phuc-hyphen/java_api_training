65b854df7c08ab29c04600106fbc009718ef9128
LE huu phuc <huu-phuc.le@epita.fr>
2022-03-15 19:01:17 +0100

    Part 3 : add Start (server) test

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -1,5 +1,8 @@
 package fr.lernejo.navy_battle;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +11,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,5 +28,34 @@ class LauncherTest {
         HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
         Assertions.assertThat(response.statusCode()).isEqualTo(200);
         Assertions.assertThat(response.body()).isEqualTo("OK");
+    }
+
+    @Test
+    void StartTests() throws IOException, InterruptedException {
+        String[] args = {"1234"};
+        String my_id = "TEst.IT";
+        String my_port = "8765";
+        Launcher.main(args);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest requetePost = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:1234/api/game/start"))
+            .setHeader("Accept", "application/json")
+            .setHeader("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"" + my_id + "\", \"url\":\"http://localhost:" + my_port + "\", \"message\":\"hello\"}"))
+            .build();
+        HttpResponse<String> response = client.send(requetePost, HttpResponse.BodyHandlers.ofString());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        StartMessage jsonMap = objectMapper.readValue(response.body(), StartMessage.class);
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(202);
+        Assertions.assertThat(jsonMap.message()).isEqualTo("May the best code win");
+        Assertions.assertThat(jsonMap.url()).isEqualTo("http://localhost:1234");
+        try{
+            UUID uuid = UUID.fromString(jsonMap.id());
+        } catch (IllegalArgumentException exception){
+            Assertions.assertThat(exception).isEqualTo(null);
+        }
     }
 }
